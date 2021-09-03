0cbbae1e83f72df6692f6d5942c72e341c5fdc2f
FernandoMarcon <fmarcon13@gmail.com>
2021-09-03 17:41:38 -0300

    com

--- Dissertation/pdf_to_markdown.py
+++ Dissertation/pdf_to_markdown.py
@@ -1,3 +1,29 @@
-import PyPDF2
+import pdftotext
+import re
 
-file = open('Tese/FernandoMarconPassosDefesaMestrado.pdf','rb')
+# PIPELINE
+# read pdf
+# extract text
+# full text
+# find topics
+# split full text by topics
+# for each topic:
+#     write a markdown file, named by topic title
+
+#--- Read PDF
+file = open('Dissertation/FernandoMarconPassosDefesaMestrado.pdf','rb')
+pdf = pdftotext.PDF(file)
+pdf = list(pdf)[:80:]
+
+#--- Extract text
+def get_page(page):
+    return page.replace('\u200b','').strip()
+
+full_text = '\n\n'.join([get_page(page) for page in pdf])
+
+#--- Find Topics
+topics_idx = {'cover':0,'cover2':1,'acknowledgements':2,'abstract':[3,4],'abstract2':[5,6],'summary':7}
+topics_idx
+
+pdf[topics_idx['summary']].encode('utf-8')
+summary = get_page(pdf[topics_idx['summary']]).split('\n')
